6b79ea2f93f5476b0b8c6663753e0c880ac08764
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-11-22 01:01:46 +0000

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+
+const App = () => {
+
+  return <h1>Hello World</h1>
+}
+
+export default App
