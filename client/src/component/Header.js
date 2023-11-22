df88c6e60b08ec55e90e0be6f6701db5a6bf29ef
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-11-22 01:22:11 +0000

    add db

---
+++ client/src/component/Header.js
@@ -0,0 +1,19 @@
+export default function Header() {
+
+  return (
+    <div className="header">
+      <h1>
+        <a href="/">Voca</a>
+      </h1>
+      <div>
+        <a href="#x" className="link">
+          Add Voca
+        </a>
+        <a href="#x" className="link">
+          Add Date
+        </a>
+      </div>
+
+    </div>
+  )
+}
